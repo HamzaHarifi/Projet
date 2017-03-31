d9395f1b07aeb97803120fa8810b0a3070b2b7e1
mafangbom <messanafan@gmail.com>
2017-03-31 18:57:54 +0200

    Saturation ; luminosite ; appliquer une teinte; fichier Strings dans values ( factorisation de code), jeter un coup d'oeil a strngs et a menu pour voir comment cela fonctionne. J'ai eu l'idee en voyant les warming avant de commit. Il serait bien que vpus le fassiez aussi avant de commit, je parler de voir les warming et erreurs, cela permet de rendre le code meilleur. j'ai pas tout fait il y en a encore a àmeliorer. Bon voila tout.

--- app/src/main/java/com/example/mafangbom/projet/MainActivity.java
+++ app/src/main/java/com/example/mafangbom/projet/MainActivity.java
@@ -36,7 +36,7 @@ import static android.graphics.Color.blue;
 import static android.graphics.Color.green;
 import static android.graphics.Color.red;
 import static android.graphics.Color.rgb;
-//
+
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     private Toolbar toolbar;
